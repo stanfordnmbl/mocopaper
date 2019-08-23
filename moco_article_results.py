8c1f7b05274667ccc513cb26cdf906ab057be1c1
Christopher Dembia <cld72@cornell.edu>
2019-08-23 12:18:40 -0700

    shift_data_to_cycle.

--- moco_article_results.py
+++ moco_article_results.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import os
+import copy
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -28,6 +29,202 @@ def toarray(simtk_vector):
     for i in range(simtk_vector.size()):
         array[i] = simtk_vector[i]
     return array
+
+def nearest_index(array, val):
+    return np.abs(array - val).argmin()
+
+def shift_data_to_cycle(
+        arbitrary_cycle_start_time, arbitrary_cycle_end_time,
+        new_cycle_start_time, time, ordinate, cut_off=True):
+    """
+    Takes data (ordinate) that is (1) a function of time and (2) cyclic, and
+    returns data that can be plotted so that the data starts at the desired
+    part of the cycle.
+    Used to shift data to the desired part of a gait cycle, for plotting
+    purposes.  Data may be recorded from an arbitrary part
+    of the gait cycle, but we might desire to plot the data starting at a
+    particular part of the gait cycle (e.g., right foot strike).
+    Another example use case is that one might have data for both right and
+    left limbs, but wish to plot them together, and thus must shift data for
+    one of the limbs by 50% of the gait cycle.
+    This method also cuts the data so that your data covers at most a full gait
+    cycle but not more.
+    The first three parameters below not need exactly match times in the `time`
+    array.
+    This method can also be used just to truncate data, by setting
+    `new_cycle_start_time` to be the same as `arbitrary_cycle_start_time`.
+    Parameters
+    ----------
+    arbitrary_cycle_start_time : float
+        Choose a complete cycle/period from the original data that you want to
+        use in the resulting data. What is the initial time in this period?
+    arbitrary_cycle_end_time : float
+        See above; what is the final time in this period?
+    new_cycle_start_time : float
+        The time at which the shifted data should start. Note that the initial
+        time in the shifted time array will regardlessly be 0.0, not
+        new_cycle_start_time.
+    time : np.array
+        An array of times that must correspond with ordinate values (see next),
+        and must contain arbitrary_cycle_start_time and
+        arbitrary_cycle_end_time.
+    ordinate : np.array
+        The cyclic function of time, values corresponding to the times given.
+    cut_off : bool, optional
+        Sometimes, there's a discontinuity in the data that prevents obtaining
+        a smooth curve if the data wraps around. In order prevent
+        misrepresenting the data in plots, etc., an np.nan is placed in the
+        appropriate place in the data.
+    Returns
+    -------
+    shifted_time : np.array
+        Same size as time parameter above, but its initial value is 0 and its
+        final value is the duration of the cycle (arbitrary_cycle_end_time -
+        arbitrary_cycle_start_time).
+    shifted_ordinate : np.array
+        Same ordinate values as before, but they are shifted so that the first
+        value is ordinate[{index of arbitrary_cycle_start_time}] and the last
+        value is ordinate[{index of arbitrary_cycle_start_time} - 1].
+    Examples
+    --------
+    Observe that we do not require a constant interval for the time:
+        >>> ordinate = np.array([2, 1., 2., 3., 4., 5., 6.])
+        >>> time = np.array([0.5, 1.0, 1.2, 1.35, 1.4, 1.5, 1.8])
+        >>> arbitrary_cycle_start_time = 1.0
+        >>> arbitrary_cycle_end_time = 1.5
+        >>> new_cycle_start_time = 1.35
+        >>> shifted_time, shifted_ordinate = shift_data_to_cycle(
+                ...     arbitrary_cycle_start_time, arbitrary_cycle_end_time,
+                ...     new_cycle_start_time,
+                ...     time, ordinate)
+        >>> shifted_time
+        array([ 0.  ,  0.05,  0.15,  0.3 ,  0.5 ])
+        >>> shifted_ordinate
+        array([3., 4., nan, 1., 2.])
+    In order to ensure the entire duration of the cycle is kept the same,
+    the time interval between the original times "1.5" and "1.0" is 0.1, which
+    is the time gap between the original times "1.2" and "1.3"; the time
+    between 1.2 and 1.3 is lost, and so we retain it in the place where we
+    introduce a new gap (between "1.5" and "1.0"). NOTE that we only ensure the
+    entire duration of the cycle is kept the same IF the available data covers
+    the entire time interval [arbitrary_cycle_start_time,
+    arbitrary_cycle_end_time].
+    """
+    # TODO gaps in time can only be after or before the time interval of the
+    # available data.
+
+    if new_cycle_start_time > arbitrary_cycle_end_time:
+        raise Exception('(`new_cycle_start_time` = %f) > (`arbitrary_cycle_end'
+                '_time` = %f), but we require that `new_cycle_start_time <= '
+                '`arbitrary_cycle_end_time`.' % (new_cycle_start_time,
+                    arbitrary_cycle_end_time))
+    if new_cycle_start_time < arbitrary_cycle_start_time:
+        raise Exception('(`new_cycle_start_time` = %f) < (`arbitrary_cycle'
+                '_start_time` = %f), but we require that `new_cycle_start_'
+                'time >= `arbitrary_cycle_start_time`.' % (new_cycle_start_time,
+                    arbitrary_cycle_start_time))
+
+
+    # We're going to modify the data.
+    time = copy.deepcopy(time)
+    ordinate = copy.deepcopy(ordinate)
+
+    duration = arbitrary_cycle_end_time - arbitrary_cycle_end_time
+
+    old_start_index = nearest_index(time, arbitrary_cycle_start_time)
+    old_end_index = nearest_index(time, arbitrary_cycle_end_time)
+
+    new_start_index = nearest_index(time, new_cycle_start_time)
+
+    # So that the result matches exactly with the user's desired times.
+    if new_cycle_start_time > time[0] and new_cycle_start_time < time[-1]:
+        ordinate[new_start_index] = np.interp(new_cycle_start_time, time,
+                ordinate)
+        time[new_start_index] = new_cycle_start_time
+
+    data_exists_before_arbitrary_start = old_start_index != 0
+    if data_exists_before_arbitrary_start:
+        #or (old_start_index == 0 and
+        #    time[old_start_index] > arbitrary_cycle_start_time):
+        # There's data before the arbitrary start.
+        # Then we can interpolate to get what the ordinate SHOULD be exactly at
+        # the arbitrary start.
+        time[old_start_index] = arbitrary_cycle_start_time
+        ordinate[old_start_index] = np.interp(arbitrary_cycle_start_time, time,
+                ordinate)
+        gap_before_avail_data = 0.0
+    else:
+        if not new_cycle_start_time < time[old_start_index]:
+            gap_before_avail_data = (time[old_start_index] -
+                    arbitrary_cycle_start_time)
+        else:
+            gap_before_avail_data = 0.0
+    data_exists_after_arbitrary_end = time[-1] > arbitrary_cycle_end_time
+    # TODO previous: old_end_index != (len(time) - 1)
+    if data_exists_after_arbitrary_end:
+        #or (old_end_index == (len(time) - 1)
+        #and time[old_end_index] < arbitrary_cycle_end_time):
+        time[old_end_index] = arbitrary_cycle_end_time
+        ordinate[old_end_index] = np.interp(arbitrary_cycle_end_time, time,
+                ordinate)
+        gap_after_avail_data = 0
+    else:
+        gap_after_avail_data = arbitrary_cycle_end_time - time[old_end_index]
+
+    # If the new cycle time sits outside of the available data, our job is much
+    # easier; just add or subtract a constant from the given time.
+    if new_cycle_start_time > time[-1]:
+        time_at_end = arbitrary_cycle_end_time - new_cycle_start_time
+        missing_time_at_beginning = \
+                max(0, time[0] - arbitrary_cycle_start_time)
+        move_forward = time_at_end + missing_time_at_beginning
+        shift_to_zero = time[old_start_index:] - time[old_start_index]
+        shifted_time = shift_to_zero + move_forward
+        shifted_ordinate = ordinate[old_start_index:]
+    elif new_cycle_start_time < time[0]:
+        move_forward = time[0] - new_cycle_start_time
+        shift_to_zero = time[:old_end_index + 1] - time[old_start_index]
+        shifted_time = shift_to_zero + move_forward
+        shifted_ordinate = ordinate[:old_end_index + 1]
+    else:
+        # We actually must cut up the data and move it around.
+
+        # Interval of time in
+        # [arbitrary_cycle_start_time, arbitrary_cycle_end_time] that is 'lost' in
+        # doing the shifting.
+        if new_cycle_start_time < time[old_start_index]:
+            lost_time_gap = 0.0
+        else:
+            lost_time_gap = time[new_start_index] - time[new_start_index - 1]
+
+        # Starts at 0.0.
+        if new_cycle_start_time < time[0]:
+            addin = gap_before_avail_data
+        else:
+            addin = 0
+        first_portion_of_new_time = (time[new_start_index:old_end_index+1] -
+                new_cycle_start_time + addin)
+
+        # Second portion: (1) shift to 0, then move to the right of first portion.
+        second_portion_to_zero = \
+                time[old_start_index:new_start_index] - arbitrary_cycle_start_time
+        second_portion_of_new_time = (second_portion_to_zero +
+                first_portion_of_new_time[-1] + lost_time_gap +
+                gap_after_avail_data)
+
+        shifted_time = np.concatenate(
+                (first_portion_of_new_time, second_portion_of_new_time))
+
+        # Apply cut-off:
+        if cut_off:
+            ordinate[old_end_index] = np.nan
+
+        # Shift the ordinate.
+        shifted_ordinate = np.concatenate(
+                (ordinate[new_start_index:old_end_index+1],
+                    ordinate[old_start_index:new_start_index]))
+
+    return shifted_time, shifted_ordinate
 
 def plot_joint_moment_breakdown(model, moco_traj,
                                 coord_paths, muscle_paths):
@@ -389,7 +586,35 @@ class MotionTrackingWalking(MocoPaperResult):
         self.side = 'r'
 
     def shift(self, time, y):
-        pass
+        return shift_data_to_cycle(self.initial_time, self.final_time,
+                                   self.footstrike, time, y)
+        # time = copy.deepcopy(time)
+        # y = copy.deepcopy(y)
+        # # pgc = np.linspace(0, 100, 401)
+        # index_before_strike = np.arange(len(time))[time < self.footstrike][-1]
+        # index_after_strike = index_before_strike + 1
+        # assert index_after_strike < len(time) - 1
+        #
+        # # TODO: want the greatest index less than self.footstrike.
+        # y[index_after_strike] = np.interp(self.footstrike, time, y)
+        # time[index_after_strike] = self.footstrike
+        #
+        # first_portion_of_new_time = time[index_after_strike::] - self.footstrike
+        # gap_before_footstrike = self.footstrike - time[index_before_strike]
+        # second_portion_of_new_time = (time[0:index_after_strike]
+        #                               - time[0] + first_portion_of_new_time[-1]
+        #                               + gap_before_footstrike)
+        # # print('DEBUG', first_portion_of_new_time, second_portion_of_new_time)
+        # shifted_time = np.concatenate(first_portion_of_new_time,
+        #                               second_portion_of_new_time)
+        # shifted_y = np.concatenate(y[index_after_strike::],
+        #                            y[0:index_after_strike])
+        # # TODO: final_time - initial_time?
+        # assert shifted_time[-1] == time[-1] - time[0]
+        # assert len(shifted_time) == len(time)
+        # assert len(shifted_y) == len(y)
+        # return shifted_time, shifted_y
+
 
 
     def create_model_processor(self):
@@ -419,10 +644,6 @@ class MotionTrackingWalking(MocoPaperResult):
         # TODO:
         #  - avoid removing muscle passive forces
         #  - play with weights between tracking and effort.
-        #  - look for the script that plots muscle activity for the
-        #    sit-to-stand example
-        #  - simulate many gait cycles? show that the code works fine on a suite
-        #    of subjects (5 subjects?). **create a separate git repository.
         #  - report duration to solve the problem.
         #  - figure could contain still frames of the model throughout the motion.
         #  - make sure residuals and reserves are small (generate_report).
@@ -502,33 +723,36 @@ class MotionTrackingWalking(MocoPaperResult):
         inverse.set_final_time(self.final_time)
         inverse.set_mesh_interval(0.01)
         inverse.set_kinematics_allow_extra_columns(True)
+        inverse.set_tolerance(1e-5)
         # 2 minutes
         solution = inverse.solve()
         solution.getMocoSolution().write(self.mocoinverse_solution_file)
 
         # TODO: Minimize joint reaction load!
+    def plot(self, ax, time, y, *args, **kwargs):
+        shifted_time, shifted_y = self.shift(time, y)
+        # TODO is this correct?
+        duration = self.final_time - self.initial_time
+        ax.plot(100.0 * shifted_time / duration, shifted_y, *args, **kwargs)
 
     def report_results(self):
         sol_track = osim.MocoTrajectory(self.mocotrack_solution_file)
         time_track = sol_track.getTimeMat()
-        pgc_track = 100.0 * (time_track - time_track[0]) / (
-                    time_track[-1] - time_track[0])
         sol_inverse = osim.MocoTrajectory(self.mocoinverse_solution_file)
         time_inverse = sol_inverse.getTimeMat()
-        pgc_inverse = 100.0 * (time_inverse - time_inverse[0]) / (
-                    time_inverse[-1] - time_inverse[0])
 
         modelProcessor = self.create_model_processor()
         model = modelProcessor.process()
         report = osim.report.Report(model, self.mocoinverse_solution_file)
         report.generate()
 
+        # TODO: slight shift in CMC solution might be due to how we treat
+        # percent gait cycle and the fact that CMC is missing 0.02 seconds.
         sol_cmc = osim.TimeSeriesTable('results/motion_tracking_walking_cmc_results/'
                                        'motion_tracking_walking_cmc_states.sto')
         # TODO uh oh overwriting the MocoInverse solution.
         # sol_inverse.setStatesTrajectory(sol_cmc)
         time_cmc = np.array(sol_cmc.getIndependentColumn())
-        pgc_cmc = 100.0 * (time_cmc - time_cmc[0]) / (time_cmc[-1] - time_cmc[0])
 
         fig = plot_joint_moment_breakdown(model, sol_inverse,
                                     ['/jointset/hip_r/hip_flexion_r',
@@ -552,7 +776,8 @@ class MotionTrackingWalking(MocoPaperResult):
                                     #  '/forceset/med_gas_r',
                                     #  '/forceset/soleus_r',
                                     #  '/forceset/tib_ant_r'])
-        fig.show()
+        fig.savefig('joint_moment_breakdown')
+        # fig.show()
 
         fig = plt.figure(figsize=(5.5, 5.5))
         gs = gridspec.GridSpec(9, 2)
@@ -572,15 +797,16 @@ class MotionTrackingWalking(MocoPaperResult):
 
             y_cmc = coord[2] * np.rad2deg(
                 toarray(sol_cmc.getDependentColumn(f'{coord[0]}/value')),)
-            ax.plot(pgc_cmc, y_cmc, label='CMC', color='gray')
+            self.plot(ax, time_cmc, y_cmc, label='CMC', color='gray')
 
             y_inverse = coord[2] * np.rad2deg(
                 sol_inverse.getStateMat(f'{coord[0]}/value'))
-            ax.plot(pgc_inverse, y_inverse, label='MocoInverse', color='k', linestyle='--')
+            self.plot(ax, time_inverse, y_inverse, label='MocoInverse',
+                      color='k', linestyle='--')
 
             # y_track = coord[2] * np.rad2deg(
             #     sol_track.getStateMat(f'{coord[0]}/value'))
-            # ax.plot(pgc_track, y_track, label='MocoTrack', color='blue', linestyle='--')
+            # self.plot(ax, time_track, y_track, label='MocoTrack', color='blue', linestyle='--')
 
             ax.set_xlim(0, 100)
             if ic == 1:
@@ -620,12 +846,12 @@ class MotionTrackingWalking(MocoPaperResult):
         for im, muscle in enumerate(muscles):
             ax = plt.subplot(gs[im, 1])
             activation_path = f'/forceset/{muscle[0]}_{self.side}/activation'
-            ax.plot(pgc_cmc, toarray(sol_cmc.getDependentColumn(activation_path)),
+            self.plot(ax, time_cmc, toarray(sol_cmc.getDependentColumn(activation_path)),
                     color='gray')
-            ax.plot(pgc_inverse, sol_inverse.getStateMat(activation_path),
+            self.plot(ax, time_inverse, sol_inverse.getStateMat(activation_path),
                     label='Inverse',
                     color='k', linestyle='--')
-            # ax.plot(pgc_track, sol_track.getStateMat(activation_path),
+            # self.plot(ax, time_track, sol_track.getStateMat(activation_path),
             #         label='Track',
             #         color='blue', linestyle='--')
             ax.set_ylim(0, 1)

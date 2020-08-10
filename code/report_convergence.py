5fa48a4a838db1076bcb55d6c8774ae42ef98841
Christopher Dembia <cld72@cornell.edu>
2020-08-10 13:01:16 -0700

    Prepare report_convergence.py for publication-quality figures.

--- code/report_convergence.py
+++ code/report_convergence.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import numpy as np
 import pylab as pl
 import os
 import opensim as osim
 
 import utilities
-from squat_to_stand import SquatToStand
+from prescribed_walking import MotionPrescribedWalking
 from tracking_walking import MotionTrackingWalking
+from squat_to_stand import SquatToStand
 
 def get_convergence_results(root_dir, result):
     metadata = result.convergence_metadata()
@@ -15,6 +17,9 @@ def get_convergence_results(root_dir, result):
     for md in metadata:
         solution_fpath = os.path.join(root_dir, 'results', 'convergence',
                                       md['solution_file'])
+        if not os.path.exists(solution_fpath):
+            print(f"Warning: solution {solution_fpath} does not exist.")
+            continue
         table = osim.TimeSeriesTable(solution_fpath)
         num_mesh_intervals = md['num_mesh_intervals']
         # All problems use Hermite-Simpson transcription.
@@ -23,29 +28,52 @@ def get_convergence_results(root_dir, result):
         # TODO: num_mesh_intervals = (table.getNumRows() - 1) / 2
         x.append(num_mesh_intervals)
         costs.append(float(table.getTableMetaDataAsString('objective')))
+    # Normalize costs.
+    if costs:
+        costs = np.array(costs) / costs[-1]
     return x, costs
 
 def report_convergence(root_dir):
 
-    fig = pl.figure(figsize=(5.2, 2.7))
+    fig = pl.figure(figsize=(5.2, 2.5))
+    ymax = 1.2
+    yticks = [0, 1.0]
+
+    # prescribed-walking
+    # ------------------
+    x, costs = get_convergence_results(root_dir, MotionPrescribedWalking())
+    ax = fig.add_subplot(1, 3, 1)
+    ax.set_title('MocoInverse\n(Fig 7, black)')
+    ax.semilogx(x, costs, linestyle='', marker='.', color='black')
+    ax.set_xlabel('number of mesh intervals')
+    ax.set_xticks([3, 100])
+    ax.set_yticks(yticks)
+    ax.set_ylim(0, ymax)
+    utilities.publication_spines(ax)
 
     # tracking-walking
     # ----------------
     x, costs = get_convergence_results(root_dir, MotionTrackingWalking())
     ax = fig.add_subplot(1, 3, 2)
-    ax.set_title('MocoTrack')
-    ax.semilogx(x, costs, linestyle='', marker='o')
+    ax.set_title('MocoTrack\n(Fig 8)')
+    ax.semilogx(x, costs, linestyle='', marker='.', color='black')
     ax.set_xlabel('number of mesh intervals')
+    ax.set_ylim(0, ymax)
+    ax.set_yticks(yticks)
+    ax.set_yticklabels([])
+    utilities.publication_spines(ax)
 
     # squat-to-stand
     # --------------
     x, costs = get_convergence_results(root_dir, SquatToStand())
     ax = fig.add_subplot(1, 3, 3)
-    ax.set_title('squat-to-stand')
-    ax.semilogx(x, costs, linestyle='', marker='o')
+    ax.set_title('Predictive MocoStudy\n(Fig 10, black)')
+    ax.semilogx(x, costs, linestyle='', marker='.', color='black')
     ax.set_xlabel('number of mesh intervals')
-    ax.set_ylim(1.0, 1.7)
-
+    ax.set_ylim(0, ymax)
+    ax.set_yticks(yticks)
+    ax.set_yticklabels([])
+    utilities.publication_spines(ax)
 
     fig.tight_layout()
     utilities.savefig(fig, os.path.join(root_dir, 'figures', 'S3_Fig'))
